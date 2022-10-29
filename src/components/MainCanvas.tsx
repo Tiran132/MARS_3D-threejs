0eadd1f9f6cea9783b1664bdef9928ed7cf69191
Tiran132 <32066820+Tiran132@users.noreply.github.com>
2022-10-29 21:15:12 +0700

    comands in app

--- src/components/MainCanvas.tsx
+++ src/components/MainCanvas.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import {
   CommandType,
+  CommandTypeArgs,
   CommandVector3,
   CustomElementTypes,
 } from "../types/types";
@@ -40,10 +41,10 @@ function AnimatedBox({ position }: AnimatedBoxProps) {
 const zeroVector3 = new THREE.Vector3(0, 0, 0);
 const onesVector3 = new THREE.Vector3(1, 1, 1);
 const defaultRotation = new THREE.Euler(0, 0, 0);
-const defaultColor = new THREE.Color(50, 168, 82);
+const defaultColor = new THREE.Color(50 / 255, 168 / 255, 82 / 255);
 
 const str: "cube" | "sphere" = "cube";
-const defaultGeometry: [x: number, y: number, z: number] = [1, 1, 1]
+const defaultGeometry: [x: number, y: number, z: number] = [1, 1, 1];
 
 const defaultParams = {
   object_type: str,
@@ -55,7 +56,7 @@ const defaultParams = {
 };
 
 interface CustomElementType {
-    data: CustomElementTypes
+  data: CustomElementTypes;
 }
 
 function CustomElement({ data: props }: CustomElementType) {
@@ -75,6 +76,7 @@ function CustomElement({ data: props }: CustomElementType) {
   const [isCurrent, setIsCurrent] = useState(false);
 
   useEffect(() => {
+    console.log("effect");
     setIsCurrent(false);
     setObject_type(props.object_type || object_type);
     setPosition(props.position || position);
@@ -82,11 +84,13 @@ function CustomElement({ data: props }: CustomElementType) {
     setGeomety(props.geometry || geometry);
     setScale(props.scale || scale);
     setColor(props.color || color);
-  }, [props]);
+  }, [props.position]);
 
   useFrame(() => {
     if (isCurrent) return;
     if (!ref.current) return;
+
+    console.log("frame");
 
     ref.current.position.set(position.x, position.y, position.z);
     ref.current.rotation.set(rotation.x, rotation.y, rotation.z);
@@ -114,10 +118,10 @@ function CustomElement({ data: props }: CustomElementType) {
 }
 
 const ElementIndexed = (props: CustomElementTypes, id: number) => {
-    const createdElement = <CustomElement data={props} />
+  const createdElement = <CustomElement data={props} />;
 
-    return [createdElement, id]
-}
+  return [createdElement, id];
+};
 
 const BoxPos = new THREE.Vector3(0, 0, 0);
 const BoxPos1 = new THREE.Vector3(0, 1, 0);
@@ -128,10 +132,18 @@ const MainCanvas = () => {
   const [boxPos2, setBoxPos2] = useState<THREE.Vector3>(BoxPos2);
 
   const [createdObjects, setCreatedObjects] = useState<any[]>([]);
-  const [objectsData, setObjectsData] = useState<(typeof defaultParams)[]>([]);
+  const [objectsData, setObjectsData] = useState<typeof defaultParams[]>([
+    {
+      object_type: str,
+      position: zeroVector3.set(0, 1, 0),
+      rotation: defaultRotation,
+      geometry: defaultGeometry, // Размеры в 3 плоскостях
+      scale: onesVector3,
+      color: defaultColor,
+    },
+  ]);
 
-
-useEffect(() => {
+  useEffect(() => {
     const ws = new WebSocket("ws://localhost:5000/");
     ws.onopen = () => {
       console.log("Opened session");
@@ -154,19 +166,39 @@ useEffect(() => {
         const command: CommandType = JSON.parse(ev.data);
 
         if (createdObjects.length < 0) {
-            createdObjects.push(defaultParams)
+          createdObjects.push(defaultParams);
         }
 
-        if (objectsData.length > 0){
-            const c = command.args;
-            // if ("object_type" in c) objectsData[0].object_type = c.object_type;
-            if ("position" in c) objectsData[0].position = new THREE.Vector3(c.position![0], c.position![1], c.position![2])
-            if ("rotation" in c) objectsData[0].rotation = new THREE.Euler(c.rotation![0], c.rotation![1], c.rotation![2])
-            if ("geometry" in c) objectsData[0].geometry = [c.scale![0], c.scale![1], c.scale![2]]
-            if ("scale" in c) objectsData[0].scale = new THREE.Vector3(c.scale![0], c.scale![1], c.scale![2])
-            if ("color" in c) objectsData[0].color = new THREE.Color(c.color![0], c.color![1], c.color![2])
-            
-        } 
+        if (objectsData.length > 0) {
+          const c = command.args;
+          // if ("object_type" in c) objectsData[0].object_type = c.object_type;
+          if ("position" in c)
+            objectsData[0].position = new THREE.Vector3(
+              c.position![0],
+              c.position![1],
+              c.position![2]
+            );
+          if ("rotation" in c)
+            objectsData[0].rotation = new THREE.Euler(
+              c.rotation![0],
+              c.rotation![1],
+              c.rotation![2]
+            );
+          if ("geometry" in c)
+            objectsData[0].geometry = [c.scale![0], c.scale![1], c.scale![2]];
+          if ("scale" in c)
+            objectsData[0].scale = new THREE.Vector3(
+              c.scale![0],
+              c.scale![1],
+              c.scale![2]
+            );
+          if ("color" in c)
+            objectsData[0].color = new THREE.Color(
+              c.color![0],
+              c.color![1],
+              c.color![2]
+            );
+        }
 
         // switch (command.type) {
         //     case "create":
@@ -186,47 +218,87 @@ useEffect(() => {
         // }
       } catch {}
     };
-  }, []); 
+  }, []);
 
-
- const addShape = () => {
-    const [newElement, id] = ElementIndexed(defaultParams, 1)
-    console.log(newElement)
+  const addShape = () => {
+    const [newElement, id] = ElementIndexed(defaultParams, 1);
+    console.log(newElement);
     setCreatedObjects([
-        ...createdObjects,
-        newElement
-        // <CustomElement data={defaultParams} />
-    ])
- }
+      ...createdObjects,
+      newElement,
+      // <CustomElement data={defaultParams} />
+    ]);
+  };
 
- const addCommand = () => {
-    // console.log(v)
-    const command: typeof defaultParams = JSON.parse(v)
+  const addCommand = () => {
+    const command: CommandTypeArgs = JSON.parse(v);
 
-    console.log(command)
- }
+    if (objectsData.length > 0) {
+      const c = command;
+    //   const currentObjectData = 
+      let updated = objectsData[0]
+      
+    
+      // if ("object_type" in c) objectsData[0].object_type = c.object_type;
+      if ("position" in c)
+        updated.position = new THREE.Vector3(
+          c.position![0],
+          c.position![1],
+          c.position![2]
+        );
+      if ("rotation" in c)
+        updated.rotation = new THREE.Euler(
+          c.rotation![0],
+          c.rotation![1],
+          c.rotation![2]
+        );
+      if ("geometry" in c)
+        updated.geometry = [c.scale![0], c.scale![1], c.scale![2]];
+      if ("scale" in c)
+        updated.scale = new THREE.Vector3(
+          c.scale![0],
+          c.scale![1],
+          c.scale![2]
+        );
+      if ("color" in c)
+        updated.color = new THREE.Color(
+          c.color![0],
+          c.color![1],
+          c.color![2]
+        );
+      setObjectsData([updated])
+        // render()
+      // if ("position" in c) objectsData[0].position = new THREE.Vector3(c.position.x, c.position.y, c.position.z)
+      // if ("rotation" in c) objectsData[0].rotation = new THREE.Euler(c.rotation.x, c.rotation.y, c.rotation.z)
+      // if ("geometry" in c) objectsData[0].geometry = [...c.geometry]; // [c.geometry[0], c.geometry[1], c.geometry[2]]
+      // if ("scale" in c) objectsData[0].scale = new THREE.Vector3(c.scale.x, c.scale.y, c.scale.z)
+      // if ("color" in c) objectsData[0].color = new THREE.Color(c.color)
+    }
+  };
 
- const [v, setV] = useState(
+  const [v, setV] = useState(
     `{
-        object_type: "cube",
-        position: [0,0,0],
-        rotation: [0,0,0],
-      }`
- )
+        "object_type":"cube",
+        "position":[0, 0, 0],
+        "rotation":[0, 0, 0]
+     }`
+  );
 
- 
- const handleChange = (event: any) => {
+  const handleChange = (event: any) => {
     setV(event.target.value);
   };
 
   return (
     <>
+      <button onClick={() => addShape()}>Добавить объект</button>
+      <button onClick={() => addCommand()}>Команда</button>
 
-    <button onClick={() => addShape()}>Добавить объект</button>
-    <button onClick={() => addCommand()}>Команда</button>
+      <textarea
+        onChange={(e) => handleChange(e)}
+        style={{ height: "100px", width: "50%" }}
+        value={v}
+      />
 
-    <textarea onChange={(e) => handleChange(e)}  style={{height: "100px", width: "50%"}} value={v} />
-    
       <Canvas
         style={{ height: "100vh", width: "100vw" }}
         camera={{ position: [0, 2, 5], zoom: 1 }}
@@ -237,7 +309,10 @@ useEffect(() => {
         {/* <AnimatedBox position={boxPos1} />
         <AnimatedBox position={boxPos2} /> */}
 
-        {[...createdObjects]}
+        {objectsData.map((data) => {
+          console.log(data);
+          return <CustomElement data={data} />;
+        })}
 
         <Box
           onClick={() => {
--- src/types/types.ts
+++ src/types/types.ts
@@ -12,15 +12,18 @@ export type CommandVector3 = [x: number, y: number, z: number]
 export interface CommandType {
     object_id: number;
     type: "create" | "update" | "join" | "remove";
-    args: {
-        object_type: "cube" | "sphere"
-        position?: CommandVector3
-        rotation?: CommandVector3
-        geometry?: CommandVector3 // Размеры в 3 плоскостях
-        scale?:	   CommandVector3
-        color?:    CommandVector3
-        parentId?: number
-    }
+    args: CommandTypeArgs
+}
+
+export interface CommandTypeArgs {
+    id: number
+    object_type: "cube" | "sphere"
+    position?: CommandVector3
+    rotation?: CommandVector3
+    geometry?: CommandVector3 // Размеры в 3 плоскостях
+    scale?:	   CommandVector3
+    color?:    CommandVector3
+    parentId?: number
 }
 
 export interface CustomElementTypes {
